d8c4cdcf56d05053570b4d515f3a5f9c9c2e0473
Luciano Édipo <luciano.psilva@anpd.gov.br>
2025-04-29 00:51:09 -0300

    feat: add ecosystem configuration for process management

---
+++ ecosystem.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: "cadastro-controladores-api",
+      script: "dist/main.js",
+      cwd: "/home/anpdadmin/cadastro-controladores-api",
+      instances: 1,
+      exec_mode: "fork",
+      watch: false,
+      env: {
+        NODE_ENV: "production",
+        PORT: 3001,
+        CONTROLADORES_API_URL: "https://hml-dim.anpd.gov.br:3001"
+      },
+      error_file: "logs/error.log",
+      out_file: "logs/output.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss",
+    }
+  ]
+}
